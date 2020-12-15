4dc2d5dbbf17310f2b08c4146da844b49adcb7e3
M <hailstonem@users.noreply.github.com>
2020-12-15 13:10:05 +0000

    Puzzle 2 part 2

--- Puzzle2/solve.py
+++ Puzzle2/solve.py
@@ -11,6 +11,11 @@ def verify_count(text, letter, min_count, max_count):
     return actual_count >= min_count and actual_count <= max_count
 
 
+def verify_position(text, letter, pos1, pos2, count):
+    true_positions = sum([text[pos1 - 1] == letter, text[pos2 - 1] == letter])
+    return true_positions == count
+
+
 with open("input.txt", "r") as numbersfile:
     lineslist = numbersfile.readlines()
 
@@ -18,5 +23,10 @@ accumulator = 0
 for l in lineslist:
     text, letter, min_count, max_count = format_line(l)
     accumulator += verify_count(text, letter, min_count, max_count)
+print(f"Part one: Matched: {accumulator}")
 
-print(accumulator)
+accumulator = 0
+for l in lineslist:
+    text, letter, pos1, pos2 = format_line(l)
+    accumulator += verify_position(text, letter, pos1, pos2, 1)
+print(f"Part two: Matched: {accumulator}")
